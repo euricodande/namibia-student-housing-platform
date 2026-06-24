7e8dc5bb0dc2e03e12c93a20e795d943007ab7a8
Eurico Dande <223134902@nust.na>
2026-06-24 16:07:40 +0200

    Set up Spring Boot backend with Docker PostgreSQL

---
+++ backend/src/test/java/io/github/euricodande/housing/HousingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.euricodande.housing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
